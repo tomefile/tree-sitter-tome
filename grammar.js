16bef9353d42a0d8fe5fd72f1cbab916f0a9a610
BubbleFish <daforsastudia@gmail.com>
2025-11-15 13:22:27 +0300

    feat: Days of debugging to make this work but it does.

--- grammar.js
+++ grammar.js
@@ -8,10 +8,89 @@
 // @ts-check
 
 module.exports = grammar({
-  name: "tomefile",
+	name: "tome",
 
-  rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+	extras: ($) => [$._tab, $.comment],
+
+	rules: {
+		source_file: ($) =>
+			seq(
+				// Added in case the file begins with ';', which would cause everything else to error
+				repeat(";"),
+				optional($.shebang),
+				repeat($._statement)
+			),
+
+		shebang: (_) => token(seq(/#!.*/, token.immediate("\n"))),
+
+		comment: (_) => token(seq("#", /[^\n]*/, token.immediate("\n"))),
+
+		block: ($) => seq("{", repeat($._statement), "}"),
+
+		_tab: (_) => token(choice(" ", "\t")),
+
+		_statement: ($) =>
+			seq(
+				choice("\n", $.directive, $.command, seq("|", $.command)),
+				repeat(";")
+			),
+
+		directive: ($) =>
+			prec.right(
+				seq(":", $.identifier, repeat($.argument), optional($.block))
+			),
+
+		command: ($) =>
+			prec.right(
+				seq(
+					$.identifier,
+					optional("!"),
+					" ",
+					repeat($.argument),
+					repeat(seq("\\\n", $.argument))
+				)
+			),
+
+		subcommand: ($) => seq("$(", $.command, ")"),
+
+		argument: ($) =>
+			choice(
+				$.subcommand,
+				$.string,
+				$.identifier,
+				$.variable,
+				$.variable_expansion,
+				$.comparator
+			),
+
+		string: ($) =>
+			choice(
+				seq(
+					'"',
+					repeat(choice(/[^"]/, $.variable, $.variable_expansion)),
+					'"'
+				),
+				seq(
+					"'",
+					repeat(choice(/[^']/, $.variable, $.variable_expansion)),
+					"'"
+				),
+				seq("`", repeat(choice(/[^`\\]/, /\\./)), "`")
+			),
+
+		identifier: (_) => /[\w\d\-\_@\?\.]+/,
+
+		variable: (_) => token(seq("$", token.immediate(/[\w\d\-\_]+/))),
+		modifier: () => token.immediate(/[^\n}]+/),
+
+		variable_expansion: ($) =>
+			seq(
+				"${",
+				$.identifier,
+				optional(seq(token.immediate(":"), $.modifier)),
+				token.immediate("}")
+			),
+
+		comparator: (_) => token(choice("==", ">=", "<=", "<", ">")),
+	},
 });
